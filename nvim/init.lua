f6011ad4f159a606bf086f8cbb22c21ecc3237c7
Juan Valdivia <juan.valdivia@darwinhomes.com>
2025-01-22 09:37:43 -0600

    update keybindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,7 +42,7 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find f
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-vim.keymap.set("n", "<leader>or", telescope.extensions.recent_files.pick, { desc = "Telescope recent files" })
+vim.keymap.set("n", "<leader>fr", telescope.extensions.recent_files.pick, { desc = "Telescope recent files" })
 
 -- Neotree
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>")
